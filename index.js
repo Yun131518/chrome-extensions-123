fb5bad463db2d90a08f123ad14be269c229d50fe
Yun131518 <ykim68268@gmail.com>
2025-05-17 21:23:06 +0900

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://i.dell.com/sites/csimages/App-Merchandizing_esupport_flatcontent_global_Images/all/bluescreen.png'})
+
+
+}, 500)
+
+
